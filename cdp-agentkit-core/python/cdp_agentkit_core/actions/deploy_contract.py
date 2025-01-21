80de9852a1c0cb1ff2da4fc1a368dff6687599ab
Rohan Agarwal <rohan.agarwal.1@coinbase.com>
2025-01-21 11:03:43 -0500

    Fix

--- cdp-agentkit-core/python/cdp_agentkit_core/actions/deploy_contract.py
+++ cdp-agentkit-core/python/cdp_agentkit_core/actions/deploy_contract.py
@@ -2,22 +2,33 @@ from collections.abc import Callable
 
 from cdp import Wallet
 from pydantic import BaseModel, Field
+from typing import Any
 
-from cdp_agentkit_core.actions import CdpAction
+from python.cdp_agentkit_core.actions import CdpAction
+
 
 DEPLOY_CONTRACT_PROMPT = """
-This tool will deploy an arbitrary contract. It takes a map of arbitrary customer arguments as input.
-The token will be deployed using the wallet's default address.
+This tool will deploy an arbitrary smart contract. It takes 3 required arguments: solidity compiler version which is a string, solidity input json which is a string, 
+contract name which is a string, and 1 optional argument: constructor arguments which is a map of string to any.
+The solidity version must be 0.8.+, such as "0.8.28+commit.7893614a" which is the latest version. See https://binaries.soliditylang.org/bin/list.json for valid versions.
+The input json must be a valid solidity input json. See https://docs.soliditylang.org/en/latest/using-the-compiler.html#input-description for more details. If you 
+depend on a library, make sure to include the library source code as separate sources inline in the input json.
+For the settings outputSelection in the input JSON, make sure to have a string array including the abi and evm.bytecode.
+The contract name must be the name of the contract class to be deployed.
+The constructor arguments, if passed in, must be a map of constructor arguments for the contract, where the key is the argument name and the value is the argument value.
+uint, int, bytes, fixed bytes, string, address should be encoded as strings. Boolean values should be encoded as true or false.
+For arrays and tuples, the values should be encoded depending on the underlying type contained in the array or tuple.
 """
 
 
-class DeployTokenInput(BaseModel):
+class DeployContractInput(BaseModel):
     """Input argument schema for deploy token action."""
 
-    name: str = Field(..., description='The name of the token (e.g., "My Token")')
-    symbol: str = Field(..., description='The token symbol (e.g., "USDC", "MEME", "SYM")')
-    total_supply: str = Field(
-        ..., description='The total supply of tokens to mint (e.g., "1000000")'
+    solidity_version: str = Field(..., description='The solidity compiler version')
+    solidity_input_json: str = Field(..., description='The input json for the solidity compiler')
+    contract_name: str = Field(..., description='The name of the contract class to be deployed')
+    constructor_args: dict[str, Any] = Field(
+        ..., description='The constructor arguments for the contract'
     )
 
 
