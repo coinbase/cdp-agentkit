2d082bbd46b46257aab8b2976ddcea573ed78cce
Christopher Gerber <csgerber@gmail.com>
2025-01-22 15:03:41 -0800

    removing code comment

--- cdp-agentkit-core/typescript/src/actions/cdp/defi/morpho/deposit.ts
+++ cdp-agentkit-core/typescript/src/actions/cdp/defi/morpho/deposit.ts
@@ -70,7 +70,6 @@ export async function depositToMorpho(
   try {
     const tokenAsset = await Asset.fetch(wallet.getNetworkId(), args.tokenAddress);
     const atomicAssets = tokenAsset.toAtomicAmount(assets);
-    // const atomicAssets = BigInt(tokenAsset.toAtomicAmount(assets).toString());
 
     const approvalResult = await approve(
       wallet,
