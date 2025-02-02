18a7afd224bb0724c1b7d15a728c06a73f3626cf
ngundotra <noah@gundotra.org>
2025-02-02 20:03:18 +0000

    add network constants

---
+++ typescript/agentkit/src/network/svm.ts
@@ -0,0 +1,8 @@
+export const SOLANA_MAINNET_CHAIN_ID = 101;
+export const SOLANA_DEVNET_CHAIN_ID = 103;
+
+export const SOLANA_MAINNET_NETWORK_ID = "solana-mainnet";
+export const SOLANA_DEVNET_NETWORK_ID = "solana-devnet";
+
+export const SOLANA_PROTOCOL_FAMILY = "svm";
+export const SOLANA_MAINNET_GENESIS_BLOCK_HASH = "4sGjMW1sUnHzSxGspuhpqLDx6wiyjNtZAMdL4VZHirAn";
