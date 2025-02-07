1298d043e2e49b0fd14ebaf692ad14a21c9fcbca
arnavmehta7 <arnavmehta.cs@gmail.com>
2025-02-08 03:22:57 +0530

    changed logic to support both current wallet + other wallets which user may prompt

--- python/cdp-agentkit-core/cdp_agentkit_core/actions/get_balance.py
+++ python/cdp-agentkit-core/cdp_agentkit_core/actions/get_balance.py
@@ -1,12 +1,13 @@
 from collections.abc import Callable
+from typing import Optional
 
-from cdp import Wallet
+from cdp import Cdp, Wallet
 from pydantic import BaseModel, Field
 
 from cdp_agentkit_core.actions import CdpAction
 
 GET_BALANCE_PROMPT = """
-This tool will get the balance of all the addresses in the wallet for a given asset.
+This tool will get the balance of address.
 It takes the asset ID as input. Always use 'eth' for the native asset ETH and 'usdc' for USDC.
 """
 
@@ -18,10 +19,14 @@ class GetBalanceInput(BaseModel):
         ...,
         description="The asset ID to get the balance for, e.g. `eth`, `0x036CbD53842c5426634e7929541eC2318f3dCF7e`",
     )
+    address_to_fund: Optional[str] = Field(
+        None,
+        description="The address to fund. If not provided, the default address of the wallet will be used.",
+    )
 
 
-def get_balance(wallet: Wallet, asset_id: str) -> str:
-    """Get balance for all addresses in the wallet for a given asset.
+def get_balance(wallet: Wallet, asset_id: str, address_to_fund: Optional[str]) -> str:
+    """Get balance for the address defined
 
     Args:
         wallet (Wallet): The wallet to get the balance for.
@@ -33,13 +38,17 @@ def get_balance(wallet: Wallet, asset_id: str) -> str:
     """
     # for each address in the wallet, get the balance for the asset
     balances = {}
-
-    try:
-        for address in wallet.addresses:
-            balance = address.balance(asset_id)
-            balances[address.address_id] = balance
-    except Exception as e:
-        return f"Error getting balance for all addresses in the wallet {e!s}"
+    if address_to_fund:
+        balances[address_to_fund] = Cdp.api_clients.external_addresses.get_external_address_balance(
+            network_id=wallet.network_id, address_id=address_to_fund, asset_id=asset_id
+        )
+    else:
+        try:
+            for address in wallet.addresses:
+                balance = address.balance(asset_id)
+                balances[address.address_id] = balance
+        except Exception as e:
+            return f"Error getting balance for all addresses in the wallet {e!s}"
 
     # Format each balance entry on a new line
     balance_lines = [f"  {addr}: {balance}" for addr, balance in balances.items()]
