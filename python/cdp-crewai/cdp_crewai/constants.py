42ea02050d24789e3f5e39bc37fee9cc4f1e00d7
bilgin-kocak <kocakbilgin@gmail.com>
2025-02-08 17:09:11 +0300

    feat: init and constant added for cdp crewai

---
+++ python/cdp-crewai/cdp_crewai/constants.py
@@ -0,0 +1,4 @@
+"""Specifies package level constants used throughout the package."""
+
+# CDP_CREWAI_DEFAULT_SOURCE (str): Denotes the default source for CDP CrewAI extensions.
+CDP_CREWAI_DEFAULT_SOURCE = "cdp-crewai"
