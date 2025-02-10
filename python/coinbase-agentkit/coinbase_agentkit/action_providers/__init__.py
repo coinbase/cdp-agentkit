999300cade9015162e07fbd57adc00c19ddc701c
Christopher Gerber <csgerber@gmail.com>
2025-02-10 13:23:57 -0800

    exporting

--- python/coinbase-agentkit/coinbase_agentkit/action_providers/__init__.py
+++ python/coinbase-agentkit/coinbase_agentkit/action_providers/__init__.py
@@ -2,6 +2,7 @@ from .action_decorator import create_action
 from .action_provider import Action, ActionProvider
 from .morpho.morpho_action_provider import MorphoActionProvider, morpho_action_provider
 from .pyth.pyth_action_provider import PythActionProvider, pyth_action_provider
+from .wallet.wallet_action_provider import WalletActionProvider, wallet_action_provider
 
 __all__ = [
     "Action",
@@ -11,4 +12,6 @@ __all__ = [
     "pyth_action_provider",
     "MorphoActionProvider",
     "morpho_action_provider",
+    "WalletActionProvider",
+    "wallet_action_provider",
 ]
