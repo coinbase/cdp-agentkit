daab3f37d138614cb508e03ec554253672d43b33
Christopher Gerber <csgerber@gmail.com>
2025-02-10 22:22:50 -0800

    exporting

--- python/coinbase-agentkit/coinbase_agentkit/wallet_providers/__init__.py
+++ python/coinbase-agentkit/coinbase_agentkit/wallet_providers/__init__.py
@@ -1,3 +1,4 @@
+from .cdp_wallet_provider import CdpProviderConfig
 from .eth_account_wallet_provider import EthAccountWalletProvider, EthAccountWalletProviderConfig
 from .evm_wallet_provider import EvmWalletProvider
 from .wallet_provider import WalletProvider
@@ -5,6 +6,7 @@ from .wallet_provider import WalletProvider
 __all__ = [
     "WalletProvider",
     "EvmWalletProvider",
+    "CdpProviderConfig",
     "EthAccountWalletProvider",
     "EthAccountWalletProviderConfig",
 ]
