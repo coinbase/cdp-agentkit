f3893575737d139fd590858fdf6587d2cfa67599
Christopher Gerber <csgerber@gmail.com>
2025-02-10 16:41:36 -0800

    fix

--- python/coinbase-agentkit/coinbase_agentkit/action_providers/weth/constants.py
+++ python/coinbase-agentkit/coinbase_agentkit/action_providers/weth/constants.py
@@ -12,19 +12,4 @@ WETH_ABI = [
         "stateMutability": "payable",
         "type": "function",
     },
-    {
-        "inputs": [
-            {
-                "name": "account",
-                "type": "address",
-            },
-        ],
-        "outputs": [
-            {
-                "type": "uint256",
-            },
-        ],
-        "stateMutability": "view",
-        "type": "function",
-    },
 ]
